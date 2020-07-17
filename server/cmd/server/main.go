b66ef4f82a18892af0bb3b67f5a0cd0be39dae8a
John Kinsella <jlkinsel@gmail.com>
2020-07-17 11:59:44 -0700

    disableCors() wasn't being used.

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -16,22 +16,3 @@ func main() {
 		logrus.Errorf("Error when attempting to start network server: %s", err.Error())
 	}
 }
-
-func disableCors(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, Accept-Encoding")
-
-		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Max-Age", "86400")
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		h.ServeHTTP(w, r)
-
-	})
-}
