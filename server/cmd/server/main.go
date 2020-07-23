e140f31d6c837bc17eb56d355c0ec5fbb6c4c1dd
John Kinsella <jlkinsel@gmail.com>
2020-07-23 08:01:15 -0700

    Adding CORS support to server

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jlk/webapp-base/server/controllers"
 	"github.com/jlk/webapp-base/server/data"
@@ -10,6 +11,13 @@ import (
 
 func main() {
 	ginRouter := gin.Default()
+	ginRouter.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"http://localhost:8080"},
+		AllowMethods:  []string{"GET"},
+		AllowHeaders:  []string{"Origin"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
+
 	// Database connection happens in data/postgres.go init()
 
 	ginRouter.GET("/devices", controllers.GetDevices)
--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,7 @@ module github.com/jlk/webapp-base/server
 go 1.14
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jackc/pgx/v4 v4.7.2
 	github.com/jmoiron/sqlx v1.2.0
