a671f328695f6cc24f2d3db8e4ffc0076967b832
John Kinsella <jlkinsel@gmail.com>
2020-07-23 08:30:50 -0700

    Using vue-cli to "add" babel
    looks like it was already here, but not fully set up

--- coreui-client/babel.config.js
+++ coreui-client/babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'entry',
-        corejs: 3
-      }
-    ]
+    '@vue/cli-plugin-babel/preset'
   ]
 }
